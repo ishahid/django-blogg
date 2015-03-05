cec5f2d6f77fd41772f8c2a0a727663352c3c80d
Imran Shahid <narmi79@gmail.com>
2015-03-05 19:37:01 +1100

    added comments model
    - the comment model, migration and admin inlines
    - improvement to post admin
    - added tag admin
    - added comments to the post template

--- source/blogg/models.py
+++ source/blogg/models.py
@@ -47,3 +47,22 @@ class Post(models.Model):
     def increment_views(self):
         self.views += 1
         self.save()
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments')
+    content = models.TextField(max_length=1000L)
+    author = models.CharField(max_length=100, default="Anonymous", null=False, blank=True)
+    ip_address = models.GenericIPAddressField(null=True, blank=True)
+    user_agent = models.CharField(max_length=500L, null=False, blank=True)
+    published = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now_add=True, auto_now=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    objects = CustomQuerySet.as_manager()
+
+    def __unicode__(self):
+        return u'%s' % self.pk
